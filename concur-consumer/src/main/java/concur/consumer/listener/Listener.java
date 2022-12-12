6b07f943c9a7391205758c953d3afb2e0580bdf1
injae-ju <injae.ju@navercorp.com>
2022-12-12 15:33:03 +0900

    test

--- concur-consumer/src/main/java/concur/consumer/listener/Listener.java
+++ concur-consumer/src/main/java/concur/consumer/listener/Listener.java
@@ -1,25 +1,32 @@
 package concur.consumer.listener;
 
-import concur.core.config.KafkaCount;
-import concur.core.entity.User;
-import concur.core.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import concur.core.config.KafkaCount;
+import concur.core.entity.User;
+import concur.core.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class Listener {
 
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
+	private final RedisTemplate<String, String> redisTemplate;
 
-    @Transactional
-    @KafkaListener(topics = "count", groupId = "1")
-    public void countTopicListener(KafkaCount kafkaCount) {
-        User user = userRepository.findById(kafkaCount.getUserId()).orElseThrow();
-        user.setCnt(kafkaCount.getCount());
-    }
+	@Transactional
+	@KafkaListener(topics = "count", groupId = "1")
+	public void countTopicListener(KafkaCount kafkaCount) {
+		// 차라리 redis에서 값을 가져와서 업데이트 치는게 나을 듯.
+		ValueOperations<String, String> valueOp = redisTemplate.opsForValue();
+		User user = userRepository.findById(kafkaCount.getUserId()).orElseThrow();
+		String s = valueOp.get(user.getName());
+		user.setCnt(Integer.parseInt(s));
+		userRepository.save(user);
+	}
 
 }
